b91c2d4f41c27d519ba987a8c3e3fc10b415aee8
carlos <caarlos.albuquerq@gmail.com>
2022-09-19 17:11:35 -0300

    test: add account already exists and login with invalid data

--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -2,6 +2,8 @@ import { prisma } from './../src/database/db';
 import app from '../src/index.js';
 import supertest from "supertest";
 import * as userFactory from "./userFactory.js";
+import { encryptAccountPassword } from '../src/utils/passwordUtils';
+import { createUser } from '../src/repositories/authRepository';
 
 const agent = supertest(app);
 
@@ -39,13 +41,34 @@ describe("POST /signup", () => {
 
         expect(result.status).toBe(422);
     });
+
+    it("Create a account already exists 409", async () => {
+        const user = userFactory.user();
+        const body = { ...user, confirmPassword: user.password };
+    
+        await supertest(app).post("/signup").send(body);
+        const result = await agent.post("/signup").send(body);
+    
+        const status = result.status;
+        expect(status).toBe(409);
+      });
 });
 
 describe("POST /signin", () => {
-    it("login whith nonexistent email must return statuscode 422", async () => {
+    it("login with nonexistent email must return status code 422", async () => {
         const user = userFactory.user();
         const result = await agent.post("/signin").send(user);
 
         expect(result.status).toBe(401);
-    })
-})
+    });
+
+    it("login with valid data must return status code 200", async () => {
+        const user = userFactory.user();
+        const add = createUser(user);
+
+        const result = await agent.post("/signin").send(user);
+
+        expect(result.status).toBe(401);
+    });
+
+});
